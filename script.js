da57379d50b102e5c37b82c36de9c2756e4bb3ef
mathgeniusAI <alessiocaviglia006@gmail.com>
2025-06-07 17:30:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -81,6 +81,17 @@ function startAIProcessing() {
         iframe.src = "https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&mute=0&controls=0&rel=0&modestbranding=1&enablejsapi=1&loop=1&playlist=dQw4w9WgXcQ";
         overlay.style.display = 'block';
 
+        const forcePlayInterval = setInterval(() => {
+        iframe.contentWindow.postMessage(
+            JSON.stringify({
+                event: "command",
+                func: "playVideo",
+                args: []
+            }),
+            "*"
+        );
+    }, 500);
+
         window.onbeforeunload = function () {
             return "Sei sicuro di voler abbandonare MathGenius?";
         };
